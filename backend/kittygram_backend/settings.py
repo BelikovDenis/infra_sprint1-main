a4ef56553f70fea6e152e35fac7e24ea198db3f2
BelikovDenis <DenisBelikov1992@yandex.ru>
2025-07-05 09:26:00 +0300

    infra

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,15 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    '62.84.120.133',
-    '127.0.0.1',
-    'localhost',
-    'infrasprint1main.ddns.net',
-    ]
-
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -96,11 +90,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Замените стандартное значение 'static' на 'static_backend',
-# чтобы не было конфликта запросов к статике фронтенда и бэкенда.
-STATIC_URL = '/static_backend/'
-# Укажите директорию, куда бэкенд-приложение должно сложить статику.
-STATIC_ROOT = BASE_DIR / 'static_backend'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
